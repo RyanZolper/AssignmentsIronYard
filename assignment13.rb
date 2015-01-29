40cb0b746f10c0423224f51dad3e616f1a9b77f7
Ryan Zolper <zolper11@gmail.com>
2015-01-29 13:33:41 -0500

    submit 13

---
+++ assignment13.rb
@@ -0,0 +1,21 @@
+puts "Guess a number from 1 to 15"
+
+a = 4
+
+r = gets.chomp.to_i
+
+count = 1
+
+until r == a
+
+puts 'nope'
+  if r < a
+    puts 'too low bro'
+  else puts 'too high, tough guy'
+  end
+  puts 'Guess again, chump.'
+  r = gets.chomp.to_i
+  count += 1
+end
+
+puts "Congratulations! You guessed #{count} times! You suck!"
