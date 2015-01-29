0a1dc04212b4833cd4369648abbde5a896ce7132
Ryan Zolper <zolper11@gmail.com>
2015-01-28 20:28:41 -0500

    wow i am dumb

--- assignment10.rb
+++ assignment10.rb
@@ -2,10 +2,16 @@ print "Type a string please"
 
 puts string = gets.chomp
 
+words = string.split.size
+
+puts words
+
 puts string.lstrip.rstrip!
 
 puts string.reverse!
 
 puts string.chop!
 
-puts string.reverse
+puts string.reverse!
+
+puts words
