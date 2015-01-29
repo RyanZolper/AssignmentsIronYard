ae91d4fb7c1dcb11ed19b86be4d6909709e98265
Ryan Zolper <zolper11@gmail.com>
2015-01-29 13:33:25 -0500

    submit 12

---
+++ assignment12.rb
@@ -0,0 +1,19 @@
+qs = ["what's your name?", "who's your daddy?", "is he rich like me?"]
+
+puts qs[0]
+a0 = gets.chomp
+
+puts qs[1]
+a1 = gets.chomp
+
+puts qs[2]
+a2 = gets.chomp
+
+puts qs[0]
+puts a0
+
+puts qs[1]
+puts a1
+
+puts qs[2]
+puts a2
