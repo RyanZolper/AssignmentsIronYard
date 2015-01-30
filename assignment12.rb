f59fa17216da6f2f0aea94f38c0d75c1b3fb5a2a
Ryan Zolper <zolper11@gmail.com>
2015-01-30 09:13:32 -0500

    submitting 12

--- assignment12.rb
+++ assignment12.rb
@@ -1,7 +1,23 @@
 qs = ["what's your name?", "who's your daddy?", "is he rich like me?"]
 
+r = 0
+x = 0
+
+ary = Array.new
+
 qs.each do |q|
   print q
   a = gets.chomp
-  a = a.to_i + 1
+
+  ary[x] = a
+  x += 1
+
+end
+
+s = 0
+
+qs.each do |q|
+  puts q
+  puts ary[s]
+  s += 1
 end
