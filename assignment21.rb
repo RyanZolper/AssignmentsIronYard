6d7d795b240cc75285a87d3aff7492dc2509b215
Ryan Zolper <zolper11@gmail.com>
2015-02-05 14:41:50 -0500

    21

--- assignment21.rb
+++ assignment21.rb
@@ -1,46 +1,4 @@
-class Person
-  attr_reader :first_name, :last_name
-  attr_accessor :birthday
 
-
-
-  def name(input)
-    namearray = input.split
-    @first_name = namearray[0]
-    @last_name = namearray[1]
-  end
-
-  require 'Date'
-  require 'active_support'
-  require 'active_support/all'
-
-  def bd(bdinput)
-    @birthday = Date.parse(bdinput)
-  end
-
-  count = 0
-
-  @@trynacount = 0
-
-
-  def self.personcount
-    @@trynacount +=1
-  end
-
-  def findage
-    calculateAge
-  end
-
-private
-
-
-  def calculateAge
-    dob = Date.current - @birthday
-    age = dob.to_i / 365
-  end
-
-
-end
 
 
 nameinput = "nothing"
