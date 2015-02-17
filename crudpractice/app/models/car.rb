f044c9a07b649ed4c5a6547f4aff8f132c7c8fec
Ryan Zolper <zolper11@gmail.com>
2015-02-17 14:42:01 -0500

    crudd

--- crudpractice/app/models/car.rb
+++ crudpractice/app/models/car.rb
@@ -1,2 +1,14 @@
+# == Schema Information
+#
+# Table name: cars
+#
+#  id         :integer          not null, primary key
+#  make       :string
+#  model      :string
+#  HP         :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Car < ActiveRecord::Base
 end
--- crudpractice/test/models/car_test.rb
+++ crudpractice/test/models/car_test.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: cars
+#
+#  id         :integer          not null, primary key
+#  make       :string
+#  model      :string
+#  HP         :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 require 'test_helper'
 
 class CarTest < ActiveSupport::TestCase
